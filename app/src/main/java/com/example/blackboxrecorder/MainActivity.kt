7ef33f2d177c767d8ff93455e5146a4b20842a10
NDolphin1224 <teahun2@gmail.com>
2026-09-24 17:10:28 +0900

    권한설정, 메인코드, 래퍼 제작

---
+++ app/src/main/java/com/example/blackboxrecorder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.blackboxrecorder
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 화면 UI 코드는 나중에 추가할 예정
+    }
+}
